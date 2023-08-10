a7e51cfae36c1c45c02ac18b6460e80bb82a303b
RehamS21 <reham.mu433@gmail.com>
2023-08-10 18:30:59 +0300

    Update ProductService.java

--- Project3_RehamAlmutairi/src/main/java/com/example/project3_rehamalmutairi/Service/ProductService.java
+++ Project3_RehamAlmutairi/src/main/java/com/example/project3_rehamalmutairi/Service/ProductService.java
@@ -9,35 +9,6 @@ import java.util.ArrayList;
 @Service
 public class ProductService {
     ArrayList<Product> products = new ArrayList<>();
-
-    public ArrayList<Product> getAllProducts() {
-
-        return products;
-    }
-
-    public void addProduct(Product product) {
-        products.add(product);
-    }
-
-    public boolean updateProduct(Integer id,Product product){
-        for (int i = 0; i < products.size(); i++) {
-            if(products.get(i).getId() == id){
-                products.set(i , product);
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public boolean deleteProduct(Integer id){
-        for (int i = 0; i < products.size(); i++) {
-            if(products.get(i).getId() == id){
-                products.remove(i);
-                return true;
-            }
-        }
-        return false;
-    }
+    
 
 }
