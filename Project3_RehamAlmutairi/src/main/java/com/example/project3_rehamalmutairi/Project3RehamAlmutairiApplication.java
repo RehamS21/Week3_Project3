a132728e30a0d7793c994e855f06953d46d4863a
RehamS21 <reham.mu433@gmail.com>
2023-08-10 13:37:02 +0300

    Project3

---
+++ Project3_RehamAlmutairi/src/main/java/com/example/project3_rehamalmutairi/Project3RehamAlmutairiApplication.java
@@ -0,0 +1,13 @@
+package com.example.project3_rehamalmutairi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3RehamAlmutairiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project3RehamAlmutairiApplication.class, args);
+	}
+
+}
---
+++ Project3_RehamAlmutairi/src/test/java/com/example/project3_rehamalmutairi/Project3RehamAlmutairiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project3_rehamalmutairi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3RehamAlmutairiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
